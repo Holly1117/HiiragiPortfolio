2c3e01a3e3970a15a0cdad7f61d6087e3fc09fd3
Holly1117 <79395160+Holly1117@users.noreply.github.com>
2024-06-28 07:49:43 +0900

    no message

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Works from "./pages/Works";
 import Arts from "./pages/Arts";
+import ScrollToTop from "./components/ScrollToTop";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Container } from "@chakra-ui/react";
@@ -10,6 +11,7 @@ import { Container } from "@chakra-ui/react";
 const App: React.FC = () => {
   return (
     <Router basename="/HiiragiPortfolio">
+      <ScrollToTop />
       <Header />
       <Container maxW="container.md" pt={10}>
         <Routes>
